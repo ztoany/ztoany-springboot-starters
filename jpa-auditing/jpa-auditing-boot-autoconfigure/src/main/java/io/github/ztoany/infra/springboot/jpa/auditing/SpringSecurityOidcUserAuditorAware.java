6320c7722b246b9022ac87633b50ba50e05130dc
ztoany <marvin.m.huang@outlook.com>
2023-10-12 14:50:28 +0800

    get oidc sub will return empty string or throw exception if there is no user info

--- jpa-auditing/jpa-auditing-boot-autoconfigure/src/main/java/io/github/ztoany/infra/springboot/jpa/auditing/SpringSecurityOidcUserAuditorAware.java
+++ jpa-auditing/jpa-auditing-boot-autoconfigure/src/main/java/io/github/ztoany/infra/springboot/jpa/auditing/SpringSecurityOidcUserAuditorAware.java
@@ -1,24 +1,12 @@
 package io.github.ztoany.infra.springboot.jpa.auditing;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 
 import java.util.Optional;
 
 public class SpringSecurityOidcUserAuditorAware implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.ofNullable(SecurityContextHolder.getContext())
-                .map(SecurityContext::getAuthentication)
-                .filter(Authentication::isAuthenticated)
-                .map(Authentication::getPrincipal)
-                .map(DefaultOidcUser.class::cast)
-                .map(DefaultOidcUser::getUserInfo)
-                .map(OidcUserInfo::getClaims)
-                .map(e -> (String)e.get("sub"));
+        return OidcUserUtil.getOidcSubjectIdOrEmptyStr();
     }
 }
