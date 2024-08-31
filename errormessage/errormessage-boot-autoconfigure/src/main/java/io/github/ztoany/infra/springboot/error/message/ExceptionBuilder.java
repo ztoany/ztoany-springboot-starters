d56fedb8c0675b6e3300fe5e1aa5acc2b94e4b6b
ztoany <marvin.m.huang@outlook.com>
2024-08-31 21:12:22 +0800

    add errormessage module

---
+++ errormessage/errormessage-boot-autoconfigure/src/main/java/io/github/ztoany/infra/springboot/error/message/ExceptionBuilder.java
@@ -0,0 +1,35 @@
+package io.github.ztoany.infra.springboot.error.message;
+
+import io.github.ztoany.infra.exception.BusinessException;
+import io.github.ztoany.infra.exception.SystemException;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+public class ExceptionBuilder {
+    private static MessageSource messageSource;
+
+    public static void setMessageSource(MessageSource messageSource) {
+        ExceptionBuilder.messageSource = messageSource;
+    }
+
+    public static SystemException systemException(String code) {
+        var msg = messageSource.getMessage(code, null, Locale.getDefault());
+        return new SystemException(code, msg);
+    }
+
+    public static SystemException systemException(String code, Locale locale) {
+        var msg = messageSource.getMessage(code, null, locale);
+        return new SystemException(code, msg);
+    }
+
+    public static BusinessException businessException(String code) {
+        var msg = messageSource.getMessage(code, null, Locale.getDefault());
+        return new BusinessException(code, msg);
+    }
+
+    public static BusinessException businessException(String code, Locale locale) {
+        var msg = messageSource.getMessage(code, null, locale);
+        return new BusinessException(code, msg);
+    }
+}
