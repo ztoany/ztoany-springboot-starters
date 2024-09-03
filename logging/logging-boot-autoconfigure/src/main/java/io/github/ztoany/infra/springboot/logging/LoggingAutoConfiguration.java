320c6a7b241bd7d633e5d92f9e9764d56a432619
ztoany <marvin.m.huang@outlook.com>
2024-09-03 14:45:42 +0800

    fix issue: warning message is not eligible for getting processed by all BeanPostProcessors (for example: not eligible for auto-proxying)

--- logging/logging-boot-autoconfigure/src/main/java/io/github/ztoany/infra/springboot/logging/LoggingAutoConfiguration.java
+++ logging/logging-boot-autoconfigure/src/main/java/io/github/ztoany/infra/springboot/logging/LoggingAutoConfiguration.java
@@ -1,19 +1,10 @@
 package io.github.ztoany.infra.springboot.logging;
 
-import io.github.ztoany.infra.springboot.logging.tomcat.TomcatAccessLogDefaultValueProcessor;
-import io.github.ztoany.infra.springboot.util.ApplicationNameResolver;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.coyote.UpgradeProtocol;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @AutoConfiguration
+@ComponentScan("io.github.ztoany.infra.springboot.logging")
 public class LoggingAutoConfiguration {
-    @Bean
-    @ConditionalOnClass({ Tomcat.class, UpgradeProtocol.class })
-    public TomcatAccessLogDefaultValueProcessor tomcatAccessLogDefaultValueProcessor(ApplicationNameResolver applicationNameResolver) {
-        return new TomcatAccessLogDefaultValueProcessor(applicationNameResolver);
-    }
 }
