c6a6c550a7e15d3faa6d9a0ca19badfcb7a1bace
ztoany <marvin.m.huang@outlook.com>
2024-10-30 18:16:32 +0800

    fix bug on duplicated bean when spring.messages.basename is set

--- exception/exception-boot-autoconfigure/src/main/java/io/github/ztoany/infra/springboot/exception/I18nMessageSourceAutoConfiguration.java
+++ exception/exception-boot-autoconfigure/src/main/java/io/github/ztoany/infra/springboot/exception/I18nMessageSourceAutoConfiguration.java
@@ -1,19 +1,37 @@
 package io.github.ztoany.infra.springboot.exception;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.context.MessageSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.Ordered;
 import org.springframework.util.StringUtils;
 
 import java.time.Duration;
 
-
-public class PredefinedErrorMessageAutoConfiguration extends MessageSourceAutoConfiguration {
+@AutoConfiguration
+@ConditionalOnMissingBean(name = "messageSource", search = SearchStrategy.CURRENT)
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+@EnableConfigurationProperties
+@AutoConfigureBefore(MessageSourceAutoConfiguration.class)
+public class I18nMessageSourceAutoConfiguration {
 
     @Bean
-    @Override
+    @ConfigurationProperties(prefix = "spring.messages")
+    public MessageSourceProperties messageSourceProperties() {
+        return new MessageSourceProperties();
+    }
+
+
+    @Bean
     public MessageSource messageSource(MessageSourceProperties properties) {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         if (StringUtils.hasText(properties.getBasename())) {
